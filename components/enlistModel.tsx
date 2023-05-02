dcb5032b31882e5a40a60856f6b6e1890a9ad1c1
hananAzeez <hanan.azeez112@gmail.com>
2023-05-03 02:43:49 +0530

    enlist model styling

--- components/enlistModel.tsx
+++ components/enlistModel.tsx
@@ -7,18 +7,18 @@ interface EnlistModelProps {
 const EnlistModel = ({ setShowEnlistModel }: EnlistModelProps) => {
   return (
     <div
-      className="absolute w-screen h-screen top-0 left-0 bg-black bg-opacity-50 flex items-center justify-center"
+      className="absolute w-screen h-screen top-0 left-0 bg-black bg-opacity-50 flex items-center justify-center "
       onClick={() => setShowEnlistModel(false)}
     >
-      <div className="flex gap-5 p-5 text-center font-lexend font-medium text-xl md:text-2xl lg:text-3xl bg-black rounded-md z-50">
-        <div className="enlist-company pb-14 w-64 h-[460px] flex  items-end justify-center">
-          <h2>
+      <div className="flex gap-5 p-5 text-center font-lexend font-medium text-xl md:text-2xl lg:text-3xl bg-black rounded-lg z-50 ">
+        <div className="enlist-company pb-14 w-64 h-[460px] flex  items-end justify-center hover:cursor-pointer rounded-sm hover:border hover:border-white ">
+          <h2 className="hover:underline hover:underline-offset-1 transition-all duration-300">
             Enlist <br />
             Company
           </h2>
         </div>
-        <div className="enlist-individual pb-14 w-64 flex items-end justify-center">
-          <h2>
+        <div className="enlist-individual pb-14 w-64 flex items-end justify-center hover:cursor-pointer rounded-sm hover:border hover:border-white">
+          <h2 className="hover:underline hover:underline-offset-1 transition-all duration-300">
             Enlist <br />
             Individual
           </h2>
