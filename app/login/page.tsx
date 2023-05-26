9b1ffde999baf157d14f51778a7c0774c0d3102c
hananAzeez <hanan.azeez112@gmail.com>
2023-05-26 23:55:32 +0530

    profile

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -76,110 +76,110 @@ console.log(data)
 
 
   return (
-    <main className="h-screen font-inter max-w-screen-xl mx-auto flex items-center justify-center relative">
-      <section className="">
-        <div
-          className="p-8 md:p-6 lg:p-7
-             xl:px-0 "
-        >
-          {/* <!-- back icon --> */}
-          <div>
-            <Link href="/">
-              <img
-                src="/images/back-icon.png"
-                alt="back icon"
-                className="absolute top-7 left-5 lg:left-0 w-8 lg:w-10 h-8 lg:h-10"
-              />
-            </Link>
-          </div>
-          <div className="flex items-center justify-center lg:gap-20">
-            <div className="max-w-[400px] ">
-              <p className="font-bold text-3xl ">Welcome back</p>
-              <p
-                className="font-medium text-lg md:text-xl lg:text-2xl
-                        mt-1"
-              >
-                Enter your login details
-              </p>
-              {/* <!-- login form --> */}
-              <FormProvider {...form}>
-                <form
-                  onSubmit={handleSubmit(onSubmit)}
-                  noValidate
-                  action="submit"
-                  className="mt-8 md:mt-11 lg:mt-14
-                        mb-12 md:mb-16 lg:mb-20"
-                >
-                  <div className="">
+    // <main className="h-screen font-inter max-w-screen-xl mx-auto flex items-center justify-center relative">
+    //   <section className="">
+    //     <div
+    //       className="p-8 md:p-6 lg:p-7
+    //          xl:px-0 "
+    //     >
+    //       {/* <!-- back icon --> */}
+    //       <div>
+    //         <Link href="/">
+    //           <img
+    //             src="/images/back-icon.png"
+    //             alt="back icon"
+    //             className="absolute top-7 left-5 lg:left-0 w-8 lg:w-10 h-8 lg:h-10"
+    //           />
+    //         </Link>
+    //       </div>
+    //       <div className="flex items-center justify-center lg:gap-20">
+    //         <div className="max-w-[400px] ">
+    //           <p className="font-bold text-3xl ">Welcome back</p>
+    //           <p
+    //             className="font-medium text-lg md:text-xl lg:text-2xl
+    //                     mt-1"
+    //           >
+    //             Enter your login details
+    //           </p>
+    //           {/* <!-- login form --> */}
+    //           <FormProvider {...form}>
+    //             <form
+    //               onSubmit={handleSubmit(onSubmit)}
+    //               noValidate
+    //               action="submit"
+    //               className="mt-8 md:mt-11 lg:mt-14
+    //                     mb-12 md:mb-16 lg:mb-20"
+    //             >
+    //               <div className="">
 
-                    <input
-                      // name="email"
-                      type="text"
-                      placeholder="Username"
-                      className="w-full border-b border-black border-opacity-40
-                                py-4 focus:outline-none mb-9"
-                      {...register("username")}
-                    // onBlur={() => trigger("email")}
-                    />
-                  </div>
-                  <div className="">
+    //                 <input
+    //                   // name="email"
+    //                   type="text"
+    //                   placeholder="Username"
+    //                   className="w-full border-b border-black border-opacity-40
+    //                             py-4 focus:outline-none mb-9"
+    //                   {...register("username")}
+    //                 // onBlur={() => trigger("email")}
+    //                 />
+    //               </div>
+    //               <div className="">
 
-                    < input
-                      type="password"
-                      placeholder="Password"
-                      className="w-full border-b border-black border-opacity-40
-                py-4 focus:outline-none"
+    //                 < input
+    //                   type="password"
+    //                   placeholder="Password"
+    //                   className="w-full border-b border-black border-opacity-40
+    //             py-4 focus:outline-none"
 
-                      {...register("password")}
-                    // onBlur={() => trigger("password")}
-                    />
+    //                   {...register("password")}
+    //                 // onBlur={() => trigger("password")}
+    //                 />
 
-                  </div>
+    //               </div>
 
-                  <button
-                    className=" mt-5 font-regular bg-lightOrange py-3 md:py-4 px-12 text-lg md:text-xl rounded-md w-full  hover:bg-amber-500 transition-all duration-200 active:bg-amber-700  flex justify-center  items-center"
-                    type="submit" disabled={isLoading} value={"Login"} >
+    //               <button
+    //                 className=" mt-5 font-regular bg-lightOrange py-3 md:py-4 px-12 text-lg md:text-xl rounded-md w-full  hover:bg-amber-500 transition-all duration-200 active:bg-amber-700  flex justify-center  items-center"
+    //                 type="submit" disabled={isLoading} value={"Login"} >
 
-                    {isLoading ? <TailSpin
-                      height="20"
-                      width="20"
-                      color="#fff"
-                      ariaLabel="tail-spin-loading"
-                      radius="1"
-                      wrapperStyle={{}}
-                      wrapperClass=""
-                      visible={true}
-                    /> :
-                      "Login"
-                    }
-                  </button>
-                  {/* <button className="mt-4 font-regular bg-white py-3 md:py-4 px-12 text-lg md:text-xl rounded-md w-full border border-black hover:bg-gray-200">
-                    Login with google
-                  </button> */}
+    //                 {isLoading ? <TailSpin
+    //                   height="20"
+    //                   width="20"
+    //                   color="#fff"
+    //                   ariaLabel="tail-spin-loading"
+    //                   radius="1"
+    //                   wrapperStyle={{}}
+    //                   wrapperClass=""
+    //                   visible={true}
+    //                 /> :
+    //                   "Login"
+    //                 }
+    //               </button>
+    //               {/* <button className="mt-4 font-regular bg-white py-3 md:py-4 px-12 text-lg md:text-xl rounded-md w-full border border-black hover:bg-gray-200">
+    //                 Login with google
+    //               </button> */}
 
-                </form>
-              </FormProvider>
-
-
+    //             </form>
+    //           </FormProvider>
 
 
 
-              <div className="mt-5 flex gap-1 justify-center font-bold text-base md:text-lg lg:text-xl">
-                {/* eslint-disable-next-line react/no-unescaped-entities */}
-                <p className="font-medium">Don't have an account?</p>
-                <Link href="/signup">
-                  <p>Register Now</p>
-                </Link>
-              </div>
-            </div>
-            <div className="hidden xl:block">
-              <img src="/images/loginImage.png" alt="" />
-            </div>
-          </div>
-        </div>
-      </section>
-    </main>
-    // <Profile />
+
+
+    //           <div className="mt-5 flex gap-1 justify-center font-bold text-base md:text-lg lg:text-xl">
+    //             {/* eslint-disable-next-line react/no-unescaped-entities */}
+    //             <p className="font-medium">Don't have an account?</p>
+    //             <Link href="/signup">
+    //               <p>Register Now</p>
+    //             </Link>
+    //           </div>
+    //         </div>
+    //         <div className="hidden xl:block">
+    //           <img src="/images/loginImage.png" alt="" />
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </section>
+    // </main>
+    <Profile />
   );
 };
 
