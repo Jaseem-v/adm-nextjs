6afef150762367e72d3433407c897997866b33fc
hananAzeez <hanan.azeez112@gmail.com>
2023-06-12 23:43:12 +0530

    about us page show enlist model

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -119,6 +119,7 @@ const AboutUs = () => {
         <div className="max-w-screen-xl mx-auto px-5 xl:px-0 py-10 md:py-12 xl:py-20  text-center flex flex-col items-center">
           <h3 className="text-white font-poppins font-bold text-3xl">Want to join the team?</h3>
             <button
+              onClick={() => setShowEnlistModel(true)}
               className="mt-9 font-medium bg-orange text-white py-3 px-6 rounded-lg hover:bg-opacity-90 active:translate-y-[1px] transition-all duration-75 flex items-center justify-center gap-2"
             >
               Enlist Now
@@ -126,6 +127,9 @@ const AboutUs = () => {
             </button>
         </div>
       </section>
+      {showEnlistModel && (
+          <EnlistModel setShowEnlistModel={setShowEnlistModel} />
+        )}
       {/* \\\\\\\\\\\\ */}
       {/* HEADER */}
       {/* \\\\\\\\\\\\ */}
--- components/enlistModel.tsx
+++ components/enlistModel.tsx
@@ -9,15 +9,15 @@ const EnlistModel = ({ setShowEnlistModel }: EnlistModelProps) => {
   return (
     <>
       <div
-        className="absolute w-full h-screen top-0 left-0 bg-black bg-opacity-75 flex items-center justify-center "
+        className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-75 flex items-center justify-center"
         onClick={() => setShowEnlistModel(false)}
       />
-      <div className="absolute top-0 left-0 w-full h-screen  flex items-center justify-center">
-        <div className="relative flex flex-col  md:flex-row gap-5 p-5 text-center font-lexend font-medium text-2xl lg:text-3xl bg-black rounded-lg z-50 ">
+      <div className="fixed top-0 left-0 w-full h-screen flex items-center justify-center text-white z-50">
+        <div className="relative flex flex-col md:flex-row gap-5 p-5 text-center font-lexend font-medium text-2xl lg:text-3xl bg-black rounded-lg">
           <Link
             href="/business-listings/add-company"
             onClick={() => setShowEnlistModel(false)}
-            className="enlist-company md:pb-14 w-64 h-48 md:w-56 md:h-[400px] lg:w-64 lg:h-[460px] flex  items-center md:items-end justify-center hover:cursor-pointer rounded-sm hover:border hover:border-white "
+            className="enlist-company md:pb-14 w-64 h-48 md:w-56 md:h-[400px] lg:w-64 lg:h-[460px] flex items-center md:items-end justify-center hover:cursor-pointer rounded-sm hover:border hover:border-white"
           >
             <h2 className="hover:underline hover:underline-offset-1 transition-all duration-300">
               Enlist <br />
@@ -46,12 +46,7 @@ const EnlistModel = ({ setShowEnlistModel }: EnlistModelProps) => {
             stroke="currentColor"
             aria-hidden="true"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18L18 6M6 6l12 12"
-            />
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
           {/* close button */}
         </div>
