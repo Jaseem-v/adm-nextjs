4ac76bf0a30ba16a14a239518e09bfd153bc228c
hananAzeez <hanan.azeez112@gmail.com>
2023-06-21 22:24:05 +0530

    business persons title font and button bg

--- app/businesspersons/page.tsx
+++ app/businesspersons/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import SectionHeader from "@/components/SectionHeader";
 import Link from "next/link";
@@ -8,7 +8,7 @@ import { toast } from "react-hot-toast";
 import { Data } from "../../utils/schema/stateType";
 
 const BusinessPersons = () => {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
   const [data, setData] = useState<Data[]>([]);
 
   // const fullname = data.fname + " " + data.lname;
@@ -16,18 +16,19 @@ const BusinessPersons = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        await httpClient().get(`/user/personal/verified`)
-          .then(res => {
+        await httpClient()
+          .get(`/user/personal/verified`)
+          .then((res) => {
             if (res.status === 400) {
               toast.error(res.data.message, {
-                icon: '👏',
+                icon: "👏",
               });
             }
             if (res.status === 200) {
-              console.log(res.data)
+              console.log(res.data);
               setData(res.data.data);
             }
-          })
+          });
       } catch (error) {
         console.log(error);
       }
@@ -37,19 +38,15 @@ const BusinessPersons = () => {
     fetchData();
   }, []);
 
-
   console.log(data);
-  const breadcrumbs = ['Business Persons']
+  const breadcrumbs = ["Business Persons"];
 
   return (
     <>
       {/* \\\\\\\\\\\\ */}
       {/* HEADER */}
       {/* \\\\\\\\\\\\ */}
-      <SectionHeader
-        title="Business Persons"
-        breadcrumbs={breadcrumbs}
-      />
+      <SectionHeader title="Business Persons" breadcrumbs={breadcrumbs} />
 
       {/* \\\\\\\\\\\\ */}
       {/* SECTION */}
@@ -61,12 +58,12 @@ const BusinessPersons = () => {
             flex flex-col items-start justify-center
             px-4 xl:px-0 max-w-screen-xl mx-auto"
         >
-          <p className="font-kaisei text-lg md:text-xl lg:text-2xl">
+          {/* <p className="font-kaisei text-lg md:text-xl lg:text-2xl">
             Business profiles
-          </p>
+          </p> */}
           <p
             className="font-semibold text-3xl md:text-4xl lg:text-5xl
-                mt-3 md:mt-4 lg:mt-5 text-textBlack"
+                mt-3 md:mt-4 lg:mt-5 text-textBlack font-albra"
           >
             Connect with a Network of <br /> Business Professionals
           </p>
@@ -74,7 +71,11 @@ const BusinessPersons = () => {
             className="text-sm md:text-base max-w-4xl text-start
                 mt-3 md:mt-4 lg:mt-5 font-semibold text-desc"
           >
-            Welcome to our Business Profiles section, where you can explore a diverse collection of professionals, entrepreneurs, and business leaders from various industries. Discover their stories, expertise, and achievements, and connect with like-minded individuals who share your passion for success.
+            Welcome to our Business Profiles section, where you can explore a
+            diverse collection of professionals, entrepreneurs, and business
+            leaders from various industries. Discover their stories, expertise,
+            and achievements, and connect with like-minded individuals who share
+            your passion for success.
           </p>
           {/* search div  */}
           <div
@@ -88,7 +89,7 @@ const BusinessPersons = () => {
               className="focus:outline-none"
             />
             <a href="">
-              <button className="font-regular text-white bg-orange py-3 px-4 min-[380px]:px-8 md:px-12 text-base rounded-lg hover:bg-opacity-90 active:translate-y-[1px] transition-all duration-75">
+              <button className="font-regular text-lightGold bg-brownBg py-3 px-4 min-[380px]:px-8 md:px-12 text-base rounded-lg hover:bg-opacity-90 active:translate-y-[1px] transition-all duration-75">
                 Search
               </button>
             </a>
@@ -97,8 +98,12 @@ const BusinessPersons = () => {
           {/* business persons div */}
           <div className="grid place-items-center justify-items-center w-full grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 mb-10 md:mb-14 xl:mb-20 mt-14 md:mt-16 xl:mt-20">
             {/* business person card */}
-            {data.map(person => (
-              <Link href={`/businesspersons/${person._id}`} key={person._id} className="w-full">
+            {data.map((person) => (
+              <Link
+                href={`/businesspersons/${person._id}`}
+                key={person._id}
+                className="w-full"
+              >
                 <div
                   className="py-4 md:py-5 lg:py-6  px-6 md:px-8 lg:px-10  rounded-[20px]
                     border border-black flex gap-4 items-center justify-center w-full"
@@ -117,7 +122,6 @@ const BusinessPersons = () => {
                   </div>
                 </div>
               </Link>
-
             ))}
             {/* <Link href="/businesspersons/details" className="w-full">
               <div
