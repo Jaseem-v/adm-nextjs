fb236dff9fc5e1c57b0e3a612df94f211231c64b
hananAzeez <hanan.azeez112@gmail.com>
2023-06-22 22:33:16 +0530

    advertisement page section header

---
+++ app/advertisement/page.tsx
@@ -0,0 +1,8 @@
+import SectionHeader from "@/components/SectionHeader";
+
+const Advertisement = () => {
+  const breadcrumbs = ["Advertisement"];
+  return <SectionHeader title="Advertisement" breadcrumbs={breadcrumbs} />;
+};
+
+export default Advertisement;
