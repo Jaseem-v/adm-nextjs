cda6dac57719002187b62efa25ae2ef82c9367ec
hananAzeez <hanan.azeez112@gmail.com>
2023-06-28 13:37:29 +0530

    masonry grid

--- app/advertisement/page.tsx
+++ app/advertisement/page.tsx
@@ -1,22 +1,70 @@
+"use client"
+
 import SectionHeader from "@/components/SectionHeader";
+import Masonry from 'react-masonry-css'
 
 const Advertisement = () => {
   const breadcrumbs = ["Advertisement"];
-  return (
-    <>
-      <SectionHeader title="Advertisement" breadcrumbs={breadcrumbs} />
-      <section className="my-16 max-w-7xl mx-auto">
-        <div className="ad-container grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 ">
-          <div className="rounded-lg shadow-md">
+
+  const breakpointColumnsObj = {
+    default: 3,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
+
+  const item = (
+    <div className="rounded-lg shadow-md inline-block h-fit">
             <div>
-            <img src="images/companyProfile.png" alt="c" className="rounded-t-md w-full h-full"/>
+            <img src="images/companyProfile.png" alt="c" className="rounded-t-md w-full h-full block"/>
             <div className="text p-6">
-              <h3>For sale</h3>
+              <h3 className="font-semibold text-xl">For sale</h3>
               <p className="mt-4">This item is for sale hahah now grab this oppertunity and buy this whole company</p>
             </div>
             </div>
           </div>
-        </div>
+  )
+  return (
+    <>
+      <SectionHeader title="Advertisement" breadcrumbs={breadcrumbs} />
+      <section className="my-16 max-w-7xl mx-auto">
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column">
+        {/* array of JSX items */}
+        {item}
+        {item}
+        <div className="rounded-lg shadow-md inline-block">
+            <div>
+            <img src="images/about2.png" alt="c" className="rounded-t-md w-full h-full block"/>
+            <div className="text p-6">
+              <h3 className="font-semibold text-xl">For sale</h3>
+              <p className="mt-4">This item is for sale hahah now grab this oppertunity and buy this whole company</p>
+            </div>
+            </div>
+          </div>
+        <div className="rounded-lg shadow-md inline-block">
+            <div>
+            <img src="images/header.png" alt="c" className="rounded-t-md w-full h-full block"/>
+            <div className="text p-6">
+              <h3 className="font-semibold text-xl">For sale</h3>
+              <p className="mt-4">This item is for sale hahah now grab this oppertunity and buy this whole company</p>
+            </div>
+            </div>
+          </div>
+        <div className="rounded-lg shadow-md inline-block">
+            <div>
+            <img src="images/team6.png" alt="c" className="rounded-t-md w-full h-full block"/>
+            <div className="text p-6">
+              <h3 className="font-semibold text-xl">For sale</h3>
+              <p className="mt-4">This item is for sale hahah now grab this oppertunity and buy this whole company</p>
+            </div>
+            </div>
+          </div>
+        {item}
+        {item}
+      </Masonry>
       </section>
     </>
   )
